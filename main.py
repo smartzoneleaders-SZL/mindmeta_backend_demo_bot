abbfb975aa4328a236ef0cef1abefd9cc9825283
Sarab Dar <darsarab421@gmail.com>
2025-05-05 14:00:35 +0500

    80 port

--- main.py
+++ main.py
@@ -214,5 +214,5 @@ async def websocket_endpoint(websocket: WebSocket):
         print("WebSocket already closed.")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
 
