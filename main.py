82effae726ecba82fbe8beaf90d600fc51497562
Sarab Dar <darsarab421@gmail.com>
2025-05-16 12:47:12 +0500

    cors updated

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://bot.mindmeta.ai/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
